54a273411567b6d304992e1e046d55e62953eed4
lawrencerosin <lawrencerosin@yahoo.com>
2025-12-12 21:39:52 -0500

    Added WebAssembly

--- frontend/src/current visibility.jsx
+++
@@ -1,6 +0,0 @@
-export default function CurrentVisibility({visibility}){
-    const CSS={
-        color:"white"
-    }
-    return <h1 style={CSS}>{visibility}</h1>
-}
--- frontend/src/opened file.jsx
+++
@@ -1,9 +0,0 @@
-export default function OpenedFile(){
-    const CSS={
-        color:"white"
-    }
-    if(sessionStorage.getItem("name")===null)
-        return <h1 style={CSS}>No File Opened</h1>;
-    else
-         return <h1 style={CSS}>{sessionStorage.getItem("name")}</h1>
-}
